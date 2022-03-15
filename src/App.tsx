dd651f8b89a6ab4268163845e09c2385604c1297
yorranagoncalvesgaspar <francisca.gaspar@aluno.ce.gov.br>
2022-03-15 18:29:28 -0300

    Navbar + Product Card + Catalog + react-router-dom install

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './assets/styles/custom.scss';
+import './App.css';
+import Catalog from 'pages/Catalog';
+
+const App = () => {
+  return (
+    <Catalog/>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import './styles.css';
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import Navbar from "components/Navbar";
+import ButtonIcon from 'components/Buttonicon';
+
+const Home = () => {
+    return (
+      <>
+      <Navbar/>
+      <div className="home-container">
+        <div className="base-card home-card">
+            <div className="home-content-container">
+              <div>
+                <h1>Conheça o melhor catálogo de produtos</h1>
+                <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
+                </div>
+                <ButtonIcon />
+            </div>
+            <div className="home-image-container">
+                <MainImage />
+            </div>
+
+        </div>
+
+      </div>
+      </>
+    );
+  }
+  
+  export default Home;
+  
